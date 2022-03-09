0c72493052a22fa8250ae723c843159dd6f6e54f
zihuns <seojh44@uos.ac.kr>
2022-03-09 10:00:47 +0900

    vue3-template-syntax

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1
+    :[attr]="'active'"
+    @[event]="add">
+    {{ msg }}
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'active',
+      attr: 'class',
+      event: 'click'
+    }
+  },
+  methods: {
+    add() {
+      this.msg += '!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .active {
+    color: royalblue;
+    font-size: 100px;
+  }
+</style>
